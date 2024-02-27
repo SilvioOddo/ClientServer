bb01dfb821665c24db0d5aa9f8b98f2fa90a6d91
SilvioOddo <silvioscuolaitts@gmail.com>
2024-02-27 12:41:32 +0100

    Aggiunta delle classi

---
+++ src/main/java/com/mycompany/esercizioclientserver_oddosilvio5ainf/MainClient.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.esercizioclientserver_oddosilvio5ainf;
+
+/**
+ *
+ * @author Studenti
+ */
+public class MainClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
