fa5091755591abf144e0b786ef852882b898207a
Silvio <152089840+SilvioOddo@users.noreply.github.com>
2024-03-01 15:01:16 +0100

    Aggiornamento classe: "Server.java"

--- src/main/java/com/mycompany/esercizioclientserver_oddosilvio5ainf/Server.java
+++ src/main/java/com/mycompany/esercizioclientserver_oddosilvio5ainf/Server.java
@@ -4,10 +4,54 @@
  */
 package com.mycompany.esercizioclientserver_oddosilvio5ainf;
 
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Studenti
  */
 public class Server {
+    int nPorta;
+    ServerSocket serverSocket;
+    Socket socket;
+    public Server(int nPorta){
+        this.nPorta = nPorta;
+    }
+    
+    public Socket attendi(){
+        socket = null;
+        
+        try {
+            serverSocket = new ServerSocket(nPorta);
+            System.out.println("Fase 1: Il server è in ascolto!");
+            socket = serverSocket.accept();
+            System.out.println("Fase 2: Il client ha fatto richiesta e la connessione è avvenuta!");
+        } catch (BindException ex){
+            System.err.println("Porta già in uso!");
+        } catch (IOException ex) {
+            System.err.println("Errore nella fase di ascolto del server!");
+        }
+        return socket;
+    }
+    
+    public void scrivi(){}
+    
+    public void leggi(){}
+    
+    public void chiudi(){}
+    
+    public void termina(){
+        try {
+            serverSocket.close();
+            System.out.println("L'applicazione è stata chiusa correttamente!");
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
