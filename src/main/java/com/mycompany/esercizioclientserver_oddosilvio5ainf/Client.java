65ef8a8c363da133f0740fb82c1a781eb5529e99
Silvio <152089840+SilvioOddo@users.noreply.github.com>
2024-03-20 10:48:19 +0100

    Update Client.java

--- src/main/java/com/mycompany/esercizioclientserver_oddosilvio5ainf/Client.java
+++ src/main/java/com/mycompany/esercizioclientserver_oddosilvio5ainf/Client.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.esercizioclientserver_oddosilvio5ainf;
+import java.io.*;
+import java.net.*;
 
 /**
  *
- * @author Studenti
+ * @author SO
  */
 public class Client {
+    String nomeServer = "nomeServer";
+    int nPorta = 6768;
+    DataInputStream in;
+    DataOutputStream out;
     
+    protected Socket connetti () throws IOException {
+        Socket socket = new Socket(nomeServer, nPorta);
+        
+        out = new DataOutputStream(socket.getOutputStream());
+        in = new DataInputStream(socket.getInputStream());
+        
+        return socket;
+    }
 }
